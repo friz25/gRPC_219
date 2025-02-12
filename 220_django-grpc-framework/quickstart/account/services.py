a4cf02b17d6fe69454370aae76326fc87baa8dfb
friz25 <friz25@gmail.com>
2025-02-12 12:43:23 +0200

    2.0 220_django-grpc-framework - server + client - bug: http 8000 АНЕ grpc 50051

---
+++ 220_django-grpc-framework/quickstart/account/services.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.models import User
+from django_grpc_framework import generics
+from serializers import UserProtoSerializer
+
+
+class UserService(generics.ModelService):
+    """gRPC service that allows users to be retrieved or updated."""
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserProtoSerializer
---
+++ 220_django-grpc-framework/quickstart/urls.py
@@ -0,0 +1,8 @@
+from account.protobuf import account_pb2_grpc
+from account.services import UserService
+
+urlpatterns = []
+
+
+def grpc_handlers(server):
+    account_pb2_grpc.add_UserControllerServicer_to_server(UserService.as_servicer(), server)
